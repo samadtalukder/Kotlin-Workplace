ec1bf7dbfd5ece3485775c74b0394e3b2d684663
samadtalukder <samadtalukdar6@gmail.com>
2020-07-02 12:24:08 +0600

    conditions name modify

---
+++ src/conditions/IfElse.kt
@@ -0,0 +1,19 @@
+package conditions
+
+fun main() {
+    val num = 10
+
+    /*if (num > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }*/
+
+    val result = if (num > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
+    println(result)
+}
