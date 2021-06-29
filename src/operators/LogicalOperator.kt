65104516c05d08299edbd2388cbb7c1fb989f7a1
samadtalukder <samadtalukdar6@gmail.com>
2021-06-30 00:58:58 +0600

    logical operator add

--- src/operators/LogicalOperator.kt
+++ src/operators/LogicalOperator.kt
@@ -3,5 +3,24 @@ package operators
 fun main() {
     /* &&, ||, ! */
 
+    val open = 9
+    val close = 12
+    val hour = 1
+
+    val status = if (hour >= open && hour <= close) {
+        println("Working Time")
+        true
+    } else {
+        println("Closing")
+        false
+    }
+
+    val orStatus = if (hour > open || hour < close) true else false
+
+    val notStatus = if (!(hour > open || hour < close)) true else false
+
+    println("AND Status: $status")
+    println("OR Status: $orStatus")
+    println("NOT Status: $notStatus")
 
 }
