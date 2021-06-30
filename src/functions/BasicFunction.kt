2b11bf7887b4c64b9dc517e33c7022f04425ebc1
samadtalukder <samadtalukdar6@gmail.com>
2021-06-30 22:16:41 +0600

    Basic Function program add

---
+++ src/functions/BasicFunction.kt
@@ -0,0 +1,47 @@
+package functions
+
+fun main() {
+    sumTwoNumber(11.10, 33.30)
+    val result = returnMultiplyTwoNumber(59.10, 66.69)
+    val printFullName = showName("Samad", "Talukder", 26)
+    val printSingleExpression = singleExpression(22, 9.33)
+    val default = defaultFunctionParameter("Java", 1995)
+    val printDivided = divided(10.10, 2.12)
+    val printStr = str("J", "K")
+    val printSub = showSub(10, 2)
+
+    println("Result: $result")
+    println("Name: $printFullName")
+    println("Result: $printSingleExpression")
+    println("Result: $default")
+    println("printDivided: $printDivided")
+    println("printStr: $printStr")
+    println("printSub: $printSub")
+
+}
+
+fun sumTwoNumber(x: Double, y: Double) {
+    val result = x + y
+    println("Result: $result")
+}
+
+fun returnMultiplyTwoNumber(x: Double, y: Double): Int {
+    val result = x * y
+    return result.toInt()
+}
+
+fun singleExpression(i: Int, j: Double): Double {
+    return i - j
+}
+
+fun showName(firstName: String, lastName: String, age: Int): String = "$firstName $lastName \nAge: $age"
+
+fun showSub(i: Int, j: Int): Int = i - j
+
+fun divided(x: Double, y: Double) = x / y
+
+fun str(x: String, y: String) = "$x - $y"
+
+fun defaultFunctionParameter(name: String = "Kotlin", release: Int = 2017): String {
+    return "Name: $name- Release Year: $release";
+}
